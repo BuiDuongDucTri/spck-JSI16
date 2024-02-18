929c48f846aebe58ddf5a2ed4c1a341fb48e1d52
BuiDuongDucTri <140304627+BuiDuongDucTri@users.noreply.github.com>
2024-02-18 16:01:59 +0700

    đẩy code spck buổi 11

---
+++ script/Data.js
@@ -0,0 +1,44 @@
+let shopItemsData = [
+    {
+      id: "jfhgbvnscs",
+      name: "Áo tết đỏ",
+      price: 45,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "img/aotetdo1.png",
+    },
+    {
+      id: "ioytrhndcv",
+      name: "Áo Tết xanh",
+      price: 100,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "img/aotetxanh.png",
+    },
+    {
+      id: "wuefbncxbsn",
+      name: "T Shirt",
+      price: 25,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "images/img-3.jpg",
+    },
+    {
+      id: "thyfhcbcv",
+      name: "Mens Suit",
+      price: 300,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "images/img-4.jpg",
+    },
+    {
+        id: "wuefbncxbsn",
+        name: "T Shirt",
+        price: 25,
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+        img: "images/img-3.jpg",
+      },
+      {
+        id: "thyfhcbcv",
+        name: "Mens Suit",
+        price: 300,
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+        img: "images/img-4.jpg",
+      },
+  ];
